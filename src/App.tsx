7316e2a45eef591e9494e3385a473e2eba90d26a
Eduardo Duarte <eduardoievolucao@gmail.com>
2022-07-15 22:21:25 -0300

    Create grid logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,42 @@ import logoImage from './assets/devmemory_logo.png';
 import RestartIcon from './svgs/restart.svg';
 import { Button } from './components/Button';
 import { InfoItem } from './components/InfoItem';
+import { useEffect, useState } from 'react';
+import { GridItemType } from './types/GridItemType';
+import { items } from './data/items';
 
 
 const App = () => {
+  const [playing, setPlaying] = useState<boolean>(false);
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [moveCount, setMoveCount] = useState<number>(0);
+  const [showCount, setShowCount] = useState<number>(0);
+  const [gridItems, setGridItems] = useState<GridItemType[]>([]);
+
+  useEffect(() => resetAndCreateGrid, []);
 
   const resetAndCreateGrid = () => {
+    setTimeElapsed(0);
+    setMoveCount(0)
+    setShowCount(0);
+    let tmpGrid: GridItemType[] = [];
+    for(let i = 0; i < (items.length * 2); i++) tmpGrid.push({
+        item: null, shown: false, permanentShown: false
+      });
 
+    for(let w = 0; w< 2; w++) {
+      for(let i = 0; i < items.length; i++){
+        let pos = -1;
+        while(pos < 0 || tmpGrid[pos].item !== null) {
+          pos = Math.floor(Math.random() * (items.length * 2));
+        }
+        tmpGrid[pos].item = i;
+      }
+    }
+
+    setGridItems(tmpGrid);
+
+    setPlaying(true);
   }
 
   return (
@@ -31,7 +61,9 @@ const App = () => {
 
       </C.Info>
       <C.GridArea>
-        ...
+        <C.Grid>
+
+        </C.Grid>
       </C.GridArea>
     </C.Container>
   );
